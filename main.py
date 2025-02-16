4e40d0b09e75dddebb8bdb87d270fcd220a27369
Mthbdy <baudymatheo@hotmail.com>
2025-02-16 16:16:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,5 +38,9 @@ thread = threading.Thread(target=keep_alive, daemon=True)
 thread.start()
 
 # Lancer le serveur Flask
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
